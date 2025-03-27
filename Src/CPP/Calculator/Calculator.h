1053a08b0793cc6a66cb91095603876a8b348310
Virtual Sense <virtual_sense@naver.com>
2025-03-27 16:19:10 +0900

    Calculator.h

---
+++ Src/CPP/Calculator/Calculator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Calculator {
+public:
+    double Add(double a, double b);
+    double Subtract(double a, double b);
+    double Multiply(double a, double b);
+    double Divide(double a, double b);
+};
