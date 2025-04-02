5c4d91bcd942f7ad4c6090101ddb2a34bf16814a
Virtual Sense <virtual_sense@naver.com>
2025-04-02 15:54:34 +0900

    주소록 2단계

--- Src/CPP/AddressBook/02_AddressBook.cpp
+++ Src/CPP/AddressBook/02_AddressBook.cpp
@@ -61,102 +61,105 @@ int main()
 
         switch (number)
         {
-        case 1: {
-            int no;
-            string name;
-            string phone;
+            case 1:
+            {
+                int no;
+                string name;
+                string phone;
 
-            cout << "데이터 번호를 입력하세요: ";
-            getline(cin, input);
-            no = atoi(input.c_str());
+                cout << "데이터 번호를 입력하세요: ";
+                getline(cin, input);
+                no = atoi(input.c_str());
 
-            cout << "이름을 입력하세요: ";
-            getline(cin, name);
+                cout << "이름을 입력하세요: ";
+                getline(cin, name);
 
-            cout << "전화번호를 입력하세요 (예: 010-1234-5678): ";
-            getline(cin, phone);
+                cout << "전화번호를 입력하세요 (예: 010-1234-5678): ";
+                getline(cin, phone);
 
-            Person p(no, name, phone);
-            addressbook.push_back(p);
+                Person p(no, name, phone);
+                addressbook.push_back(p);
 
-            cout << "주소록에 저장되었습니다!" << endl;
-            break;
-        }
-        case 2: {
-            cout << "삭제할 데이터 번호를 입력하세요: ";
-            getline(cin, input);
-            int delNo = atoi(input.c_str());
+                cout << "주소록에 저장되었습니다!" << endl;
+                break;
+            }
+            case 2:
+            {
+                cout << "삭제할 데이터 번호를 입력하세요: ";
+                getline(cin, input);
+                int delNo = atoi(input.c_str());
 
-            bool found = false;
-            for (auto it = addressbook.begin(); it != addressbook.end(); ++it) {
-                if (it->getNo() == delNo) {
-                    addressbook.erase(it);
-                    cout << "삭제가 완료되었습니다!" << endl;
-                    found = true;
-                    break;
+                bool found = false;
+                for (vector<Person>::iterator it = addressbook.begin(); it != addressbook.end(); ++it) {
+                    if (it->getNo() == delNo) {
+                        addressbook.erase(it);
+                        cout << "삭제가 완료되었습니다!" << endl;
+                        found = true;
+                        break;
+                    }
                 }
-            }
-            if (!found) {
-                cout << "해당 번호를 가진 데이터가 없습니다." << endl;
-            }
-            break;
-        }
-
-        case 3: {
-            if (addressbook.empty()) {
-                cout << "주소록이 비어 있습니다." << endl;
-            }
-            else {
-                cout << "\n현재 주소록 정보:" << endl;
-                for (auto& person : addressbook) {
-                    cout << "번호: " << person.getNo()
-                        << ", 이름: " << person.getName()
-                        << ", 전화번호: " << person.getPnumber() << endl;
+                if (!found) {
+                    cout << "해당 번호를 가진 데이터가 없습니다." << endl;
                 }
+                break;
             }
-            break;
-        }
-        case 4: {
-            cout << "수정할 데이터 번호를 입력하세요: ";
-            getline(cin, input);
-            int modifyNo = atoi(input.c_str());
 
-            bool found = false;
-            for (Person& person : addressbook) {
-                if (person.getNo() == modifyNo) {
-                    string newName, newPhone;
-
-                    cout << "새로운 이름을 입력하세요: ";
-                    getline(cin, newName);
-                    cout << "새로운 전화번호를 입력하세요: ";
-                    getline(cin, newPhone);
-
-                    person.setName(newName);
-                    person.setPnumber(newPhone);
-
-                    cout << "수정이 완료되었습니다!" << endl;
-                    found = true;
-                    break;
+            case 3:
+            {
+                if (addressbook.empty()) {
+                    cout << "주소록이 비어 있습니다." << endl;
                 }
+                else {
+                    cout << "\n현재 주소록 정보:" << endl;
+                    for (auto& person : addressbook) {
+                        cout << "번호: " << person.getNo()
+                            << ", 이름: " << person.getName()
+                            << ", 전화번호: " << person.getPnumber() << endl;
+                    }
+                }
+                break;
             }
+            case 4:
+            {
+                cout << "수정할 데이터 번호를 입력하세요: ";
+                getline(cin, input);
+                int modifyNo = atoi(input.c_str());
 
-            if (!found) {
-                cout << "해당 번호를 가진 데이터를 찾을 수 없습니다." << endl;
+                bool found = false;
+                for (Person& person : addressbook) {
+                    if (person.getNo() == modifyNo) {
+                        string newName, newPhone;
+
+                        cout << "새로운 이름을 입력하세요: ";
+                        getline(cin, newName);
+                        cout << "새로운 전화번호를 입력하세요: ";
+                        getline(cin, newPhone);
+
+                        person.setName(newName);
+                        person.setPnumber(newPhone);
+
+                        cout << "수정이 완료되었습니다!" << endl;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    cout << "해당 번호를 가진 데이터를 찾을 수 없습니다." << endl;
+                }
+                break;
             }
-            break;
-        }
-
-        case 5:{
-            cout << "프로그램이 안전하게 종료되었습니다!" << endl;
-            quick_exit(1);
-        }
-
+            case 5:
+            {
+                cout << "프로그램이 안전하게 종료되었습니다!" << endl;
+                quick_exit(1);
+            }
+            default:
+            {
+                cout << "올바른 메뉴를 선택하세요." << endl;
+            }
+        }//end of switch
         
-
-        default:
-            cout << "올바른 메뉴를 선택하세요." << endl;
-        }
-    }
-
+    }//end of while
     return 0;
-}
+}//end of main
